c07751f19df5d9310b789d6fc6157e0654d5d26c
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-01-27 09:08:49 -0500

    wip

--- api/routers/cot-router.js
+++ api/routers/cot-router.js
@@ -1,8 +1,14 @@
 const router = require("express").Router();
 const Cot = require("../models/cot-model");
+const axios = require("../../node_modules/axios");
 
 router.get("/report", (req, res, next) => {
-  res.json("report");
+  axios
+    .get("https://www.cftc.gov/dea/newcot/FinFutWk.txt")
+    .then((report) => {
+      res.json(report);
+    })
+    .catch(next);
 });
 
 router.get("/history/metrics", (req, res, next) => {
@@ -14,11 +20,11 @@ router.get("/history/metrics", (req, res, next) => {
 });
 
 router.get("/history/metrics/:year", (req, res, next) => {
-    Cot.getMetricsByYear(req.params.year)
+  Cot.getMetricsByYear(req.params.year)
     .then((metrics) => {
       res.json(metrics);
     })
     .catch(next);
-})
+});
 
 module.exports = router;
